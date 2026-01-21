a9c4e3a283ac127aa37aae491ddf5965bb21d891
UNBunny <nikitakuharchuk07@gmail.com>
2026-01-21 19:48:13 +0600

    fix: crop variety mapper;

--- agriculture-field-service/src/main/java/com/omstu/agriculturefield/crop/mapper/CropVarietyMapper.java
+++ agriculture-field-service/src/main/java/com/omstu/agriculturefield/crop/mapper/CropVarietyMapper.java
@@ -4,12 +4,16 @@ import com.omstu.agriculturefield.crop.dto.CropVarietyRequest;
 import com.omstu.agriculturefield.crop.dto.CropVarietyResponse;
 import com.omstu.agriculturefield.crop.model.CropVariety;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CropVarietyMapper {
+    @Mapping(target = "diseaseResistance", ignore = true)
     CropVariety toEntity(CropVarietyRequest request);
 
+    @Mapping(target = "diseaseResistance", ignore = true)
     CropVarietyResponse toResponse(CropVariety cropVariety);
 
+    @Mapping(target = "diseaseResistance", ignore = true)
     CropVariety toEntityWithId(Long id, CropVarietyRequest request);
 }
