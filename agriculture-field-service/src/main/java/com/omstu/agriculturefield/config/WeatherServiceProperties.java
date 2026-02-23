74caa09b70eaa5297744d59934ffbbf0e0313a00
UNBunny <nikitakuharchuk07@gmail.com>
2026-02-23 15:35:59 +0600

    fix: config;

--- agriculture-field-service/src/main/java/com/omstu/agriculturefield/config/WeatherServiceProperties.java
+++ agriculture-field-service/src/main/java/com/omstu/agriculturefield/config/WeatherServiceProperties.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
-@ConfigurationProperties(prefix = "external-services.weather-service")
+@ConfigurationProperties(prefix = "services.weather-service")
 public class WeatherServiceProperties {
 
     private String baseUrl = "http://localhost:8082";
